44617a90fd4d3f9fe63eb33e2c2c405b4cef3cd3
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2018-05-29 23:13:33 +0300

    Understanding it all

--- sort.bubble.js
+++ sort.bubble.js
@@ -1,25 +1,26 @@
-
 function sortBubble(arr) {
   const total = arr.length;
 
   for(let i = 0; i < total; i++) {
-    console.log('i', i, arr[i]);
+    console.log('i  '+i, '=', arr[i]);
   
-    for(let j = 0; j < total - i - 1; j++) {
-      console.log('j', j, arr[j]);
+    for(let x = 0; x < total - i - 1; x++) {
+      console.log('  ', x, '=', arr[x]);
   
-      if(arr[j] > arr[j+1]) {
-        var tmp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = tmp;
+      if(arr[x] > arr[x+1]) {
+        var tmp = arr[x];
+        arr[x] = arr[x+1]; 
+        arr[x+1] = tmp;
       }
   
     }
     
+    console.log("");
   }
 
   return arr;
 }
 
+const props = JSON.parse(process.argv[2]);
 
-console.log(sortBubble(JSON.parse(process.argv[2])));
+console.log(sortBubble(props));
